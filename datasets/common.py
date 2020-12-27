correct sf sup & enable random rotation

datasets/common.py
@@ -176,7 +176,7 @@ def get_shear_z(sx=0,sy=0):
 	return Shz
 
 def get_valid_map(new_coords,h,w):
-	return (new_coords[:,0] <= w) * (new_coords[:,1] <= h) 
+	return (new_coords[:,0] < w) * (new_coords[:,0] >= 0) * (new_coords[:,1] < h) * (new_coords[:,1] >= 0) 
 
 def perform_shearing(cam_coords_l,cam_coords_r):
 	sh = get_shear_y(random.uniform(-0.05,0.05),random.uniform(-0.05,0.05))
@@ -196,12 +196,24 @@ def perform_rotate(cam_coords_l,cam_coords_r):
 	return np.matmul(cam_coords_l,R),np.matmul(cam_coords_r,R)
 
 def perform_trans(cam_coords_l,cam_coords_r,depth_min):
-	tz = random.uniform(-depth_min/4,0)
+	tz = random.uniform(-depth_min,0)
 	T = [0,0,tz]
 	valid_area_l = cam_coords_l[:,2] >= -T[-1]
 	cam_coords_l[valid_area_l,-1] = cam_coords_l[valid_area_l,-1] + T[-1]
 	valid_area_r = cam_coords_r[:,2] >= -T[-1]
-	cam_coords_r[valid_area_r,-1] = cam_coords_l[valid_area_l,-1] + T[-1]
+	cam_coords_r[valid_area_r,-1] = cam_coords_r[valid_area_r,-1] + T[-1]
+	return cam_coords_l,cam_coords_r
+
+def perform_scaling(cam_coords_l,cam_coords_r):
+	scaling_x = random.uniform(0,0.05)
+	scaling_y = random.uniform(0,0.05)
+	scaling_z = random.uniform(0,0.05)
+	cam_coords_l[:,0] = cam_coords_l[:,0] * scaling_x
+	cam_coords_l[:,1] = cam_coords_l[:,1] * scaling_y
+	cam_coords_l[:,2] = cam_coords_l[:,2] * scaling_z
+	cam_coords_r[:,0] = cam_coords_r[:,0] * scaling_x
+	cam_coords_r[:,1] = cam_coords_r[:,1] * scaling_y
+	cam_coords_r[:,2] = cam_coords_r[:,2] * scaling_z
 	return cam_coords_l,cam_coords_r
 
 def threed_warp(image_l,depth_l,image_r,depth_r,intrinsic_l,intrinsic_r):
@@ -220,7 +232,10 @@ def threed_warp(image_l,depth_l,image_r,depth_r,intrinsic_l,intrinsic_r):
 	cam_coords_r = (inv_intrinsic_l[:3, :3] @ image_coord_r * depth_r.flatten()).T
 
 	# rotation
-	aug_cam_coords_l,aug_cam_coords_r = perform_rotate(cam_coords_l,cam_coords_r)
+	if torch.rand(1) > 0.5:
+		aug_cam_coords_l,aug_cam_coords_r = perform_rotate(cam_coords_l,cam_coords_r)
+	else: 
+		aug_cam_coords_l,aug_cam_coords_r = perform_scaling(cam_coords_l,cam_coords_r)
 	# shearing
 	aug_cam_coords_l,aug_cam_coords_r = perform_shearing(aug_cam_coords_l,aug_cam_coords_r)
 	#translation
@@ -241,9 +256,12 @@ def threed_warp(image_l,depth_l,image_r,depth_r,intrinsic_l,intrinsic_r):
 	new_img_yr = new_image_coords_r[:,1].reshape(height_r,width_r).astype(np.float32)
 
 	warpped_im_l = cv2.remap(image_l,new_img_xl,new_img_yl,cv2.INTER_LINEAR)
-	void_pixels_l = (warpped_im_l == 0).all(-1).astype(np.float32)
+	valid_pixels_l = (warpped_im_l != 0).all(-1).astype(np.float32)
 	warpped_im_r = cv2.remap(image_r,new_img_xr,new_img_yr,cv2.INTER_LINEAR)
-	void_pixels_r = (warpped_im_r == 0).all(-1).astype(np.float32)
+	valid_pixels_r = (warpped_im_r != 0).all(-1).astype(np.float32)
+
+	sf_bl = -cv2.remap(sf_l,new_img_xl,new_img_yl,cv2.INTER_LINEAR)
+	sf_br = -cv2.remap(sf_r,new_img_xr,new_img_yr,cv2.INTER_LINEAR)
 	#cv2.imwrite("l.png", warpped_im_l)
 	#cv2.imwrite("r.png", warpped_im_r)
 	#cv2.imwrite("l_og.png", image_l)
@@ -252,11 +270,13 @@ def threed_warp(image_l,depth_l,image_r,depth_r,intrinsic_l,intrinsic_r):
 	out_dict = {
 		"sf_l":sf_l,
 		"sf_r":sf_r,
+		"sf_bl":sf_bl,
+		"sf_br":sf_br,
 		"warpped_im_l":warpped_im_l,
 		"warpped_im_r":warpped_im_r,
 		"valid_l":np.expand_dims(valid_l, axis=2),
 		"valid_r":np.expand_dims(valid_r, axis=2),
-		"void_pixels_l":np.expand_dims(void_pixels_l,axis=2),
-		"void_pixels_r":np.expand_dims(void_pixels_r,axis=2)
+		"valid_pixels_l":np.expand_dims(valid_pixels_l,axis=2),
+		"valid_pixels_r":np.expand_dims(valid_pixels_r,axis=2)
 	}
 	return out_dict

datasets/kitti_raw_monosf.py
@@ -684,8 +684,9 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 		warpped_dict = threed_warp(img_list_np[0],fulldisp_list_np[0],img_list_np[1],fulldisp_list_np[2],self.intrinsic_dict_l[datename],self.intrinsic_dict_r[datename])
 		warpped_list_np = [warpped_dict["warpped_im_l"],warpped_dict["warpped_im_r"]]
 		sf_list_np = [warpped_dict["sf_l"],warpped_dict["sf_r"]]
+		sf_b_list_np = [warpped_dict["sf_bl"],warpped_dict["sf_br"]]
 		valid_sf_list_np = [warpped_dict["valid_l"],warpped_dict["valid_r"]]
-		valid_pixels_list_np = [warpped_dict["void_pixels_l"],warpped_dict["void_pixels_r"]]
+		valid_pixels_list_np = [warpped_dict["valid_pixels_l"],warpped_dict["valid_pixels_r"]]
 
 
 		k_l1 = torch.from_numpy(self.intrinsic_dict_l[datename]).float()
@@ -710,6 +711,7 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 			img_list_np = kitti_crop_image_list(img_list_np, crop_info)
 			warpped_list_np = kitti_crop_image_list(warpped_list_np,crop_info)
 			sf_list_np = kitti_crop_image_list(sf_list_np,crop_info)
+			sf_b_list_np = kitti_crop_image_list(sf_b_list_np,crop_info)
 			valid_pixels_list_np = kitti_crop_image_list(valid_pixels_list_np,crop_info)
 			valid_sf_list_np = kitti_crop_image_list(valid_sf_list_np,crop_info)
 			#disp_list_np = kitti_crop_image_list(disp_list_np, crop_info)
@@ -726,6 +728,7 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 		img_list_tensor = [self._to_tensor(img) for img in img_list_np]
 		warpped_list_tensor = [self._to_tensor(img) for img in warpped_list_np]
 		sf_list_tensor = [numpy2torch(img) for img in sf_list_np]
+		sf_b_list_tensor = [numpy2torch(img) for img in sf_b_list_np]
 		valid_sf_list_tensor = [numpy2torch(img) for img in valid_sf_list_np]
 		valid_pixels_list_tensor = [numpy2torch(img) for img in valid_pixels_list_np]
 		fulldisp_list_tensor = [numpy2torch(img) for img in fulldisp_list_np]
@@ -734,6 +737,7 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 			fulldisp_list_tensor[2*i] = self.interpolate2d_as(fulldisp_list_tensor[2*i],img_list_tensor[i])
 			warpped_list_tensor[i] = self.interpolate2d_as(warpped_list_tensor[i],img_list_tensor[i])
 			sf_list_tensor[i] = self.interpolate2d_as(sf_list_tensor[i],img_list_tensor[i])
+			sf_b_list_tensor[i] = self.interpolate2d_as(sf_b_list_tensor[i],img_list_tensor[i])
 			valid_sf_list_tensor[i] = self.interpolate2d_as(valid_sf_list_tensor[i],img_list_tensor[i])
 			valid_pixels_list_tensor[i] = self.interpolate2d_as(valid_pixels_list_tensor[i],img_list_tensor[i])
 
@@ -748,6 +752,8 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 		warpped_im_r = warpped_list_tensor[1]
 		sf_l = sf_list_tensor[0]
 		sf_r = sf_list_tensor[1]
+		sf_bl = sf_b_list_tensor[0]
+		sf_br = sf_b_list_tensor[1]
 		valid_sf_l = valid_sf_list_tensor[0]
 		valid_sf_r = valid_sf_list_tensor[1]
 		valid_pixels_l = valid_pixels_list_tensor[0]
@@ -776,6 +782,8 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 			warpped_im_r_flip = torch.flip(warpped_im_r, dims=[2])
 			sf_l_flip = torch.flip(sf_l, dims=[2])
 			sf_r_flip = torch.flip(sf_r, dims=[2])
+			sf_bl_flip = torch.flip(sf_bl, dims=[2])
+			sf_br_flip = torch.flip(sf_br, dims=[2])
 			valid_sf_l_flip = torch.flip(valid_sf_l, dims=[2])
 			valid_sf_r_flip = torch.flip(valid_sf_r, dims=[2])
 			valid_pixels_l_flip = torch.flip(valid_pixels_l, dims=[2])
@@ -795,6 +803,8 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 				"input_k_r2": k_l1,
 				"sf_l": sf_r_flip,
 				"sf_r": sf_l_flip,
+				"sf_bl": sf_br_flip,
+				"sf_br": sf_bl_flip,
 				"valid_sf_l": valid_sf_r_flip,
 				"valid_sf_r": valid_sf_l_flip,
 				"valid_pixels_l": valid_pixels_r_flip,
@@ -813,6 +823,8 @@ class KITTI_Raw_Warpping_Sf(data.Dataset):
 				"input_k_r2": k_l1,
 				"sf_l": sf_l,
 				"sf_r": sf_r,
+				"sf_bl": sf_br,
+				"sf_br": sf_bl,
 				"valid_sf_l": valid_sf_l,
 				"valid_sf_r": valid_sf_r,
 				"valid_pixels_l": valid_pixels_l,

losses.py
@@ -2445,8 +2445,8 @@ class Loss_SceneFlow_SemiDepthSup(nn.Module):
 			gt_disp_l1 = gt_disp_l1.to(disp_l1.device)
 			gt_disp_l2 = gt_disp_l2.to(disp_l1.device)
 			## Disp Loss
-			disp_occ_l1 = _adaptive_disocc_detection_disp(disp_r1).detach()
-			disp_occ_l2 = _adaptive_disocc_detection_disp(disp_r2).detach()
+			#disp_occ_l1 = _adaptive_disocc_detection_disp(disp_r1).detach()
+			#disp_occ_l2 = _adaptive_disocc_detection_disp(disp_r2).detach()
 
 
 			disp_l1_up = interpolate2d_as(disp_l1, gt_disp_l1, mode="bilinear") * w_dp
@@ -2543,7 +2543,8 @@ class Loss_SceneFlow_Sf_Sup(nn.Module):
 	def depth_loss_left_img2(self, disp_l, disp_r, img_l_aug, img_r_aug, ii, mask):
 
 		img_r_warp = _generate_image_left(img_r_aug, disp_l)
-		left_occ = (_adaptive_disocc_detection_disp(disp_r)*mask).detach()
+		left_occ = (_adaptive_disocc_detection_disp(disp_r).float()*mask).detach()
+		left_occ = left_occ == 1
 
 		## Photometric loss
 		img_diff = (_elementwise_l1(img_l_aug, img_r_warp) * (1.0 - self._ssim_w) + _SSIM(img_l_aug, img_r_warp) * self._ssim_w).mean(dim=1, keepdim=True)        
@@ -2594,44 +2595,52 @@ class Loss_SceneFlow_Sf_Sup(nn.Module):
 
 		gt_sf_l = target_dict['sf_l'].to(disp_r1_dict[0].device)
 		gt_sf_r = target_dict['sf_r'].to(disp_r1_dict[0].device)
+		gt_sf_bl = target_dict['sf_bl'].to(disp_r1_dict[0].device)
+		gt_sf_br = target_dict['sf_br'].to(disp_r1_dict[0].device)
 		gt_sf_l_mask = target_dict['valid_sf_l'].to(disp_r1_dict[0].device)
 		gt_sf_r_mask = target_dict['valid_sf_r'].to(disp_r1_dict[0].device)
 
 		gt_im_l_mask = target_dict['valid_pixels_l'].to(disp_r1_dict[0].device)
 		gt_im_r_mask = target_dict['valid_pixels_r'].to(disp_r1_dict[0].device)
 
-		for ii, (sf_f_l, sf_f_r, disp_l1, disp_l2, disp_r1, disp_r2) in enumerate(zip(output_dict['flow_f_pp'], sf_fr_dict, output_dict['disp_l1'], output_dict['disp_l2'], disp_r1_dict, disp_r2_dict)):
-			assert(sf_f_l.size()[2:4] == disp_l1.size()[2:4])
-			assert(sf_f_l.size()[2:4] == disp_l2.size()[2:4])
+		for ii, (sf_f, sf_b, disp_l1, disp_l2, disp_r1, disp_r2) in enumerate(zip(output_dict['flow_f_pp'], output_dict['flow_b_pp'], output_dict['disp_l1'], output_dict['disp_l2'], disp_r1_dict, disp_r2_dict)):
+			assert(sf_f.size()[2:4] == disp_l1.size()[2:4])
+			assert(sf_b.size()[2:4] == disp_l2.size()[2:4])
 			
 			## For image reconstruction loss
-			img_l1_aug = interpolate2d_as(target_dict["input_l1_aug"], sf_f_l)
-			img_l2_aug = interpolate2d_as(target_dict["input_l2_aug"], sf_f_l)
-			img_r1_aug = interpolate2d_as(target_dict["input_r1_aug"], sf_f_r)
-			img_r2_aug = interpolate2d_as(target_dict["input_r2_aug"], sf_f_r)
+			img_l1_aug = interpolate2d_as(target_dict["input_l1_aug"], sf_f)
+			img_l2_aug = interpolate2d_as(target_dict["input_l2_aug"], sf_f)
+			img_r1_aug = interpolate2d_as(target_dict["input_r1_aug"], sf_f)
+			img_r2_aug = interpolate2d_as(target_dict["input_r2_aug"], sf_f)
 
-			#im_mask = interpolate2d_as(gt_im_r_mask*gt_im_l_mask, disp_r1)
+			im_mask = interpolate2d_as(gt_im_r_mask*gt_im_l_mask, disp_r1).to(disp_l1.device).float()
 
 			## Disp Loss
 			loss_disp_l1, disp_occ_l1 = self.depth_loss_left_img1(disp_l1, disp_r1, img_l1_aug, img_r1_aug, ii)
-			#loss_disp_l2, disp_occ_l2 = self.depth_loss_left_img2(disp_l2, disp_r2, img_l2_aug, img_r2_aug, ii, im_mask)
-			loss_dp_sum = loss_dp_sum + (loss_disp_l1) * self._weights[ii]
+			loss_disp_l2, disp_occ_l2 = self.depth_loss_left_img2(disp_l2, disp_r2, img_l2_aug, img_r2_aug, ii, im_mask)
+			loss_dp_sum = loss_dp_sum + (loss_disp_l1 + loss_disp_l2) * self._weights[ii]
 
 
 			## Sceneflow Loss           
-			sf_f_l_up = interpolate2d_as(sf_f_l, gt_sf_l)
+			sf_f_up = interpolate2d_as(sf_f, gt_sf_l)
+			sf_b_up = interpolate2d_as(sf_b, gt_sf_l)
 			#gt_sf_r_down = interpolate2d_as(gt_sf_r, sf_f_r)
 			disp_occ_l1 = interpolate2d_as(disp_occ_l1.float(), gt_sf_l_mask)
+			disp_occ_l2 = interpolate2d_as(disp_occ_l2.float(), gt_im_l_mask)
 			#sf_r_mask_down = interpolate2d_as(gt_sf_r_mask, sf_f_r)
-			sf_mask = gt_sf_l_mask * disp_occ_l1.float()
+			sf_f_mask = gt_sf_l_mask * disp_occ_l1.float()
+			sf_b_mask = gt_im_l_mask * disp_occ_l2.float()
 
-			valid_epe_l = _elementwise_robust_epe_char(sf_f_l_up, gt_sf_l) * sf_mask
-			valid_epe_l[sf_mask == 0].detach_()
-			flow_l_loss = valid_epe_l[sf_mask != 0].mean()
+			valid_epe_f = _elementwise_robust_epe_char(sf_f_up, gt_sf_l) * sf_f_mask
+			valid_epe_f[sf_f_mask == 0].detach_()
+			flow_f_loss = valid_epe_f[sf_f_mask != 0].mean()
 
+			valid_epe_b = _elementwise_robust_epe_char(sf_b_up, gt_sf_bl) * sf_b_mask
+			valid_epe_b[sf_b_mask == 0].detach_()
+			flow_b_loss = valid_epe_b[sf_b_mask != 0].mean()
 			#valid_epe_r = _elementwise_robust_epe_char(out_flow, gt_flow) * sf_r_mask_down * disp_occ_l2
 
-			loss_sf_sum = loss_sf_sum + (flow_l_loss) * self._weights[ii]            
+			loss_sf_sum = loss_sf_sum + (flow_f_loss + flow_b_loss) * self._weights[ii]            
 
 		# finding weight
 		f_loss = loss_sf_sum.detach()